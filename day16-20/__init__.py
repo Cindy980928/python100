22dc1643e63ed35988cfcecf2bdaa89923055052
Cindy980928 <1282324479@qq.com>
2021-03-24 17:03:45 +0800

    day16-20

--- day16-20/__init__.py
+++ day16-20/__init__.py
@@ -8,6 +8,7 @@ items[3]>0,加,items[4]=5+2=7;
 items[4]>0,加,items[5]=-2+7=5;
 取最大值7
 '''
+import logging
 
 
 def maxSubArray():
@@ -53,7 +54,81 @@ def test():
 '''
 装饰器函数
 给某个已有函数增加功能
+装饰器让你在一个函数的前后去执行代码。
+菜鸟教程：https://www.runoob.com/w3cnote/python-func-decorators.html
 '''
 
+
+# python允许函数像变量一样被当成参数传递给另一个函数
+# 所以要扩展一个函数的功能而不改变该函数，可以执行如下操作
+
+def foo():
+    print('我是原函数')
+
+
+def extend_foo(func):
+    print('我可以扩展原函数')
+    func()
+
+
+# 调用时使用extend_foo(foo)
+
+# 上述方法实现了功能，但不是直接调用真正的foo函数，更进一步如下：
+def extend_foo1(func):
+    def wrapper():
+        print(func.__name__)
+        return func()
+
+    return wrapper  # extend_foo1返回wrapper函数，wrapper函数里是扩展了传入函数功能的代码
+
+
+def foo1():
+    print('I am foo1')
+
+
+# 上述方法中foo1()是不带参数的，我们可以在定义wrapper函数的时候指定参数
+def extend_foo2(func):
+    # *args表示任何多个无名参数,**kwargs表示关键字参数，它是一个dict
+    # 比如 foo('a', 1, None, a=1, b='2', c=3)
+    # 传过来就是
+    # args =  ('a', 1, None)
+    # kwargs =  {'a': 1, 'c': 3, 'b': '2'}
+    def wrapper(*args, **kwargs):
+        print(func.__name__)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def foo2(name, age=None, height=None):
+    print("I am %s, age %s, height %s" % (name, age, height))
+
+
+# 带参数的装饰器
+# 在外面再包一层，传入参数
+def use_logging(level):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if level == "warn":
+                print('warnning!')
+            else:
+                print('fine')
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@use_logging(level='warn')
+def foo3(name='foo3'):
+    print(name)
+
+
 if __name__ == '__main__':
-    test()
+    # foo1 = extend_foo1(foo1)  # 这条语句相当于  foo = wrapper
+    # foo1()  # 执行foo1()就相当于执行 wrapper()
+    foo2 = extend_foo2(foo2)
+    foo2('丁丁', 22, '2m')
+
+    foo3()
